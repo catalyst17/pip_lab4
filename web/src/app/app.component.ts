584a20f51e7b2bf12f5314f80983ff2c93db1e7f
dobrosldm <35300189+dobrosldm@users.noreply.github.com>
2018-12-16 22:59:56 +0300

    why not

--- web/src/app/app.component.ts
+++ web/src/app/app.component.ts
@@ -1,32 +1,40 @@
 import { Component } from '@angular/core';
 
-class Item{
-    purchase: string;
-    done: boolean;
-    price: number;
+class User{
+    login: string;
+    password: string;
 
-    constructor(purchase: string, price: number) {
-
-        this.purchase = purchase;
-        this.price = price;
-        this.done = false;
+    constructor(login: string, password: string) {
+        this.login = login;
+        this.password = password;
     }
 }
 
 @Component({
     selector: 'lab4-app',
-    template: `<div class="page-header">
+    templateUrl: '/index.html',
+    template: `        
+    <div class="page-header">
         <h1> Вход в систему </h1>
     </div>
     <div class="panel">
-        Логин:<br>
-        <input id="input" type="text" size="30" pInputText [(ngModel)]="text"><br>
-        Пароль:<br>
-        <input type="password" pPassword [(ngModel)]="property"/><br><br>
-        <button class="btn btn-default" (click)="login()">Войти</button>
-    </div>`
+        <form>
+            Логин:<br>
+            <input id="login" type="text" size="30" pInputText [(ngModel)]="text"><br>
+            Пароль:<br>
+            <input id="password" type="password" pPassword [(ngModel)]="property"/><br>
+            <button class="btn btn-default" type="submit" class="btn btn-success">Войти</button>
+        </form>
+    </div>
+    `
 })
 export class AppComponent {
-    login(): void {
-    }
+    //model = new User(,);
+
+    submitted = false;
+
+    onSubmit() { this.submitted = true; }
+
+    // TODO: Remove this when we're done
+    //get diagnostic() { return JSON.stringify(this.model); }
 }
